8c21010689f0d31e327237364681e412a293bca6
Zenoids <web@zenoids.com>
2023-10-31 16:25:34 +0530

    added description

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,21 +2,27 @@
 
 namespace App\Filament\Widgets;
 
+use App\Filament\Resources\StudentResource;
 use App\Models\Student;
+use App\Models\User;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Filament\Actions\Concerns\CanBeHidden;
 
 class StatsOverview extends BaseWidget
 {
+
     protected static ?int $sort=2;
-    // protected  array|string|int $columnSpan='full';
+    protected  array|string|int $columnSpan='full';
 
 
     protected function getStats(): array
     {
         return [
 
-            Stat::make('Total Students Registered',Student::count())
+            Stat::make('Total Students Registered',StudentResource::getEloquentQuery()->count())->description('with in your scope')
+            // ->visible((new User())->isMarkazAdmin() || (new User())->isSuperAdmin())
         ];
+
     }
 }
