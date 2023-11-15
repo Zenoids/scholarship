6091e07d7b4fd60eaeccc7120f3674e47d6fcfe7
Zenoids <web@zenoids.com>
2023-11-15 14:02:16 +0530

    added gtag

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -2,6 +2,15 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-6VHJGM9EB5"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-6VHJGM9EB5');
+</script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
