b7947d63f1f6176959f6d54d374304678362e2ae
Zenoids <web@zenoids.com>
2024-01-26 11:50:54 +0530

    added proper styling for pdf

--- resources/views/components/pdf-list.blade.php
+++ resources/views/components/pdf-list.blade.php
@@ -5,8 +5,11 @@
 <div class="col-2"> {{$key}} </div><div class="col-10"> {{$value}}</div> --}}
 
 
-    <li class="list-group-item">
-    <span class="col-2">{{$key}}</span>
-    <span class="col-1">:</span>
-    <span class="col-9">{{$value}}</span>
-     </li>
+<li class="list-group-item-text" >
+    <span class="text-left">{{$key}}:</span>
+
+    <span class="text-right"><b>
+        {{$value}}
+        </b>
+    </span>
+</li>
